df5cc3ba5d6f51a91e057fa2db68fefe9fb0f2f8
Luke Boggs <lukeboggs7@gmail.com>
2023-01-16 17:29:00 -0500

    Add script

---
+++ main.py
@@ -0,0 +1,54 @@
+from mpu6050 import mpu6050
+import time
+import math
+
+mpu = mpu6050(0x68)
+accel_similar_max = 10
+gyro_similar_max = 10
+
+gyro_tolerance = 0.25
+accel_tolerance = 0.25
+
+prev_accel = mpu.get_accel_data()
+accel_similar_for_count = 0
+prev_gyro = mpu.get_gyro_data()
+gyro_similar_for_count = 0
+
+while gyro_similar_for_count < gyro_similar_max and accel_similar_for_count < accel_similar_max:
+    print("Temp : "+str(mpu.get_temp()))
+    print()
+
+    accel_data = mpu.get_accel_data()
+
+    if math.isclose(accel_data['x'], prev_accel['x'], abs_tol=accel_tolerance) and math.isclose(accel_data['y'], prev_accel['y'], abs_tol=accel_tolerance) and math.isclose(accel_data['z'], prev_accel['z'], abs_tol=accel_tolerance):
+        accel_similar_for_count += 1
+    else:
+        accel_similar_for_count = 0
+
+    print("Acc X Diff: "+str(abs(accel_data['x'] - prev_accel['x'])))
+    print("Acc Y Diff: "+str(abs(accel_data['y'] - prev_accel['y'])))
+    print("Acc Z Diff: "+str(abs(accel_data['z'] - prev_accel['z'])))
+    print()
+
+    prev_accel = accel_data
+
+    gyro_data = mpu.get_gyro_data()
+
+    if math.isclose(gyro_data['x'], prev_gyro['x'], abs_tol=gyro_tolerance) and math.isclose(gyro_data['y'], prev_gyro['y'], abs_tol=gyro_tolerance) and math.isclose(gyro_data['z'], prev_gyro['z'], abs_tol=gyro_tolerance):
+        gyro_similar_for_count += 1
+    else:
+        gyro_similar_for_count = 0
+
+    print("Gyro X Diff: "+str(abs(gyro_data['x'] - prev_gyro['x'])))
+    print("Gyro Y Diff: "+str(abs(gyro_data['y'] - prev_gyro['y'])))
+    print("Gyro Z Diff: "+str(abs(gyro_data['z'] - prev_gyro['z'])))
+    print()
+    print("-------------------------------")
+
+    prev_gyro = gyro_data
+
+    time.sleep(1)
+
+print("gyro_similar_for_count: " + str(gyro_similar_for_count) +
+      " accel_similar_for_count: " + str(accel_similar_for_count))
+print("DONE!!!!!!!!! @ " + str(time.time()))
