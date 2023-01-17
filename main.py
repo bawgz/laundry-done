0ca99aed04b8e7b9253e95a15e75fb558ca5a9d8
Luke Boggs <lukeboggs7@gmail.com>
2023-01-16 19:48:50 -0500

    Keep the ting running

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import math
 import os
 from twilio.rest import Client
 
+in_cycle_sleep_seconds = 5
+out_of_cycle_sleep_seconds = 30
+
+is_in_cycle = False
+
 mpu = mpu6050(0x68)
 accel_similar_max = 6
 gyro_similar_max = 6
@@ -16,7 +21,7 @@ accel_similar_for_count = 0
 prev_gyro = mpu.get_gyro_data()
 gyro_similar_for_count = 0
 
-while gyro_similar_for_count < gyro_similar_max and accel_similar_for_count < accel_similar_max:
+while True:
     print("Temp : "+str(mpu.get_temp()))
     print()
 
@@ -49,23 +54,29 @@ while gyro_similar_for_count < gyro_similar_max and accel_similar_for_count < ac
 
     prev_gyro = gyro_data
 
-    time.sleep(5)
+    if gyro_similar_for_count < gyro_similar_max and accel_similar_for_count < accel_similar_max:
+        if is_in_cycle:
+            print("gyro_similar_for_count: " + str(gyro_similar_for_count) +
+                  " accel_similar_for_count: " + str(accel_similar_for_count))
+            print("DONE!!!!!!!!! @ " + str(time.time()))
 
-print("gyro_similar_for_count: " + str(gyro_similar_for_count) +
-      " accel_similar_for_count: " + str(accel_similar_for_count))
-print("DONE!!!!!!!!! @ " + str(time.time()))
+            account_sid = os.environ['TWILIO_ACCOUNT_SID']
+            auth_token = os.environ['TWILIO_AUTH_TOKEN']
+            from_num = os.environ['TWILIO_FROM_NUM']
+            to_num = os.environ['TWILIO_TO_NUM']
+            client = Client(account_sid, auth_token)
 
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-from_num = os.environ['TWILIO_FROM_NUM']
-to_num = os.environ['TWILIO_TO_NUM']
-client = Client(account_sid, auth_token)
+            message = client.messages \
+                .create(
+                    body='Your laundry is done!!!!!!!',
+                    from_=from_num,  # ex: '+15017122661'
+                    to=to_num  # ex: '+15558675310'
+                )
 
-message = client.messages \
-    .create(
-        body='Your laundry is done!!!!!!!',
-        from_=from_num,  # ex: '+15017122661'
-        to=to_num  # ex: '+15558675310'
-    )
+            print(message.sid)
 
-print(message.sid)
+        is_in_cycle = not is_in_cycle
+        print("Is in cycle: " + str(is_in_cycle))
+
+    time.sleep(
+        in_cycle_sleep_seconds if is_in_cycle else out_of_cycle_sleep_seconds)
